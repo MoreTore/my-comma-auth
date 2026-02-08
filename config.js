8c14dfe40726130ab59210ce2ed99c949dcaa787
MoreTore <ryleymccarroll22@gmail.com>
2026-02-08 01:06:07 +0100

    fix url

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ export const APPLE_REDIRECT_LINK = APPLE_AUTH_ENDPOINT + '?' + qs.stringify(APPL
 
 export const GITHUB_CLIENT_ID = 'Ov23liy0AI1YCd15pypf';
 export const GITHUB_AUTH_ENDPOINT = 'https://github.com/login/oauth/authorize';
-export const GITHUB_REDIRECT_URI = 'https://api.springerelectronics.com/v2/auth/h/redirect/'
+export const GITHUB_REDIRECT_URI = 'https://api.konik.com/v2/auth/h/redirect/'
 
 const GITHUB_OAUTH_PARAMS = {
   client_id: GITHUB_CLIENT_ID,
